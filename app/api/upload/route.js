b44c4f1e8756d6758f95397ab88cf8653249b93d
Eynar-Cast <eynar521@gmail.com>
2026-08-28 16:56:44 -0400

    Base de datos Blob

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -27,6 +27,7 @@ export async function POST(request) {
     const blob = await put(file.name, file, {
       access: 'public',
       addRandomSuffix: true,
+      token: process.env.BLOB_READ_WRITE_TOKEN,
     });
     return NextResponse.json(blob);
   } catch (error) {
